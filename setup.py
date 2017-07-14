5ad780820fb844393236e1eae800468c51ebdad6
Anciety <ding641880047@126.com>
2017-07-14 18:10:21 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="ancyfmtstr",
+    version="0.1",
+    description="enhanced format string generation",
+    author="Anciety",
+    author_email="ding641880047@126.com",
+    packages=['ancyfmtstr'],
+    install_requires=[
+        'pwntools'
+    ]
+)
